e8d763f2f4ad805715bd9e3ad3f57c1fa3f68385
taeminseo <taeminseo0115@gmail.com>
2024-09-30 23:03:49 +0900

    [Silver I] Title: 곱셈, Time: 96 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(recursion(a,b,c));
+    }
+
+    static long recursion(long a, long b, long c) {
+        if(b == 1) return a % c;
+
+        long temp = recursion(a, b/2, c);
+        if(b % 2 == 0) return (temp * temp) % c;
+        else return (((temp * temp) % c) * a) % c;
+    }
+}
